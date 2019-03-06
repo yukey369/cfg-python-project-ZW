1f1a1648d27b850c5d3a3a89b7469fba9600f949
python study <yukey369@outlook.com>
2019-03-06 19:41:01 +0000

    Week 4 website
    header and feedback

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def say_hello():
-  return "hello world!"
+  return render_template("index.html")
+
+@app.route("/<name>")
+def say_hello_to(name):
+  return render_template("index.html", user=name)
+
+@app.route("/feedback", methods=["POST"])
+def get_feedback():
+  # request.values is a dictionary holding any
+  # POST request data that's not already part of the URL
+  data = request.values
+
+  return render_template("feedback.html", form_data=data)
 
 app.run(debug=True)
