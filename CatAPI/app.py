81ebe9d9b655cba4ec0d08b4bbde8b2e13740148
python study <yukey369@outlook.com>
2019-03-27 18:59:03 +0000

    app 3

--- CatAPI/app.py
+++ CatAPI/app.py
@@ -46,9 +46,9 @@ def see_favourites():
     print(data)
     return render_template('favourites.html', data=data)
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
     #app.run('0.0.0.0', port=5000)
-    if 'PORT' in os.environ:
-     app.run(host='0.0.0.0', port=int(os.environ['PORT']))
-    else:
-     app.run(debug=True)
+if 'PORT' in os.environ:
+    app.run(host='0.0.0.0', port=int(os.environ['PORT']))
+else:
+    app.run(debug=True)
